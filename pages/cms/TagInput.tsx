77141eda00c78bd6766ff7b9665ccdb0961e181f
Rem Kim <kim.rmi.93@gmail.com>
2020-04-11 11:56:59 -0400

    markdown input and preview
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

---
+++ pages/cms/TagInput.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  Flex,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from "@chakra-ui/core";
+
+interface TagInputProps {
+  onAdd: Function;
+  label: string;
+  description: string;
+}
+
+const TagInput = ({ onAdd, label, description }: TagInputProps) => {
+  const [tag, setState] = useState("");
+  return (
+    <FormControl>
+      <FormLabel>{label}</FormLabel>
+      <Flex>
+        <Input
+          type="text"
+          id="tag-input"
+          aria-describeby="tag-text-input"
+          value={tag}
+          onChange={(ev) => setState(ev.target.value.trim())}
+          onKeyDown={(ev) => {
+            if (ev.keyCode === 13 || ev.keyCode === 32) {
+              if (tag && tag.length > 0) {
+                onAdd(tag);
+                setState("");
+              }
+            }
+          }}
+        />
+        <Button
+          variant="solid"
+          color="teal.500"
+          onClick={() => {
+            if (tag && tag.length > 0) {
+              onAdd(tag);
+              setState("");
+            }
+          }}
+        >
+          Add
+        </Button>
+      </Flex>
+      <FormHelperText>{description}</FormHelperText>
+    </FormControl>
+  );
+};
+
+export default TagInput;
--- pages/cms/write.tsx
+++ pages/cms/write.tsx
@@ -6,52 +6,19 @@ import {
   FormHelperText,
   Box,
   Heading,
-  Stack,
-  Tag,
-  TagIcon,
-  TagLabel,
-  Button,
+  Textarea,
+  Switch,
 } from "@chakra-ui/core";
 import Card from "src/components/Card";
 import { useState, createRef } from "react";
-
-const TagInput = ({ onAdd }) => {
-  const [tag, setState] = useState("");
-  return (
-    <Flex>
-      <Input
-        type="text"
-        id="tag-input"
-        aria-describeby="tag-text-input"
-        value={tag}
-        onChange={(ev) => setState(ev.target.value.trim())}
-        onKeyDown={(ev) => {
-          if (ev.keyCode === 13 || ev.keyCode === 32) {
-            if (tag && tag.length > 0) {
-              onAdd(tag);
-              setState("");
-            }
-          }
-        }}
-      />
-      <Button
-        variant="solid"
-        color="teal.500"
-        onClick={() => {
-          if (tag && tag.length > 0) {
-            onAdd(tag);
-            setState("");
-          }
-        }}
-      >
-        Add
-      </Button>
-    </Flex>
-  );
-};
+import TagInput from "pages/cms/TagInput";
+import ReactMardown from "react-markdown";
+import ChakraUIRenderer from "chakra-ui-markdown-renderer";
 
 const WritePage = () => {
   const [tags, setTags] = useState([]);
+  const [markdown, setMarkdown] = useState("");
+  const [showPreview, togglePreview] = useState(false);
   const addTag = (tag) => {
     setTags([...tags, tag]);
   };
@@ -78,20 +45,41 @@ const WritePage = () => {
             />
             <FormHelperText>Describe your idea a sentence</FormHelperText>
           </FormControl>
-          <FormControl w="sm" mt="2">
-            <FormLabel>Blog tags</FormLabel>
-            <Flex direction="column">
-              <TagInput onAdd={(tag) => addTag(tag)} />
-              <Stack spacing={4} isInline mt="4">
-                {tags.map((tag) => (
-                  <Tag size="md" key={`tag-${tag}`} variantColor="cyan">
-                    <TagIcon icon="add" size="12px" />
-                    <TagLabel>{tag}</TagLabel>
-                  </Tag>
-                ))}
-              </Stack>
-            </Flex>
+          <Box mt="2">
+            <TagInput
+              onAdd={(tag) => addTag(tag)}
+              label="Blog tags"
+              description="Tags helps searching"
+            />
+          </Box>
+          <FormControl mt="2">
+            <FormLabel htmlFor="markdown-preview">
+              Preview {showPreview ? "on" : "off"}
+            </FormLabel>
+            <Switch
+              id="markdown-preview"
+              value={showPreview}
+              onChange={() => togglePreview(!showPreview)}
+            />
           </FormControl>
+          {showPreview ? (
+            <Box borderTop="1px solid black" minH="420px" px="4" py="2">
+              <ReactMardown
+                renderers={ChakraUIRenderer()}
+                source={markdown}
+                escapeHtml={false}
+              />
+            </Box>
+          ) : (
+            <FormControl>
+              <Textarea
+                minH="420px"
+                maxH="1200px"
+                value={markdown}
+                onChange={(e) => setMarkdown(e.target.value)}
+              />
+            </FormControl>
+          )}
         </Flex>
       </Card>
     </Box>
