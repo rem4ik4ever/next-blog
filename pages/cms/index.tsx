6876fa2dde5fa2de6d44488c12e4a119abdade7c
Rem Kim <kim.rmi.93@gmail.com>
2020-04-14 23:44:42 -0400

    cms status and search string filtering
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

--- pages/cms/index.tsx
+++ pages/cms/index.tsx
@@ -1,16 +1,35 @@
 import ErrorPage from "next/error";
 import { GetStaticProps } from "next";
 import fetch from "node-fetch";
-import { Flex, Box, Tag, Button, IconButton } from "@chakra-ui/core";
+import {
+  Flex,
+  Box,
+  Button,
+  IconButton,
+  FormControl,
+  Input,
+  InputRightElement,
+  Icon,
+  InputGroup,
+  Select,
+} from "@chakra-ui/core";
 import Link from "next/link";
 import MyLink from "src/utils/MyLink";
 import StatusTag from "src/components/StatusTag";
+import { useBlogsFilter } from "src/cms/blogs/hooks";
+import { useState } from "react";
+import { BlogStatus } from "src/enums/BlogStatus";
 
 const CmsIndex = ({ blogs }) => {
-  console.log("Blogs", blogs);
   if (process.env.NODE_ENV !== "development") {
     return <ErrorPage statusCode={404} />;
   }
+  const [filters, setFilters] = useState({
+    search: "",
+    status: "all",
+  });
+  const { filteredBlogs } = useBlogsFilter(blogs, filters);
+  console.log("FIltered", filteredBlogs);
   return (
     <Flex direction="column">
       <Flex justify="space-around">
@@ -25,7 +44,46 @@ const CmsIndex = ({ blogs }) => {
           </Button>
         </Link>
       </Flex>
-      {blogs.map((blog, idx) => (
+      <Flex align="center" mt="4">
+        <FormControl w="full">
+          <InputGroup>
+            <Input
+              type="text"
+              name="search"
+              placeholder="Filter"
+              value={filters.search}
+              onChange={(ev) => {
+                setFilters({
+                  ...filters,
+                  search: ev.target.value,
+                });
+              }}
+            />
+            <InputRightElement
+              children={<Icon name="search" color="gray.500" />}
+            />
+          </InputGroup>
+        </FormControl>
+        <FormControl>
+          <Select
+            value={filters.status}
+            onChange={(ev) => {
+              setFilters({
+                ...filters,
+                status: ev.target.value,
+              });
+            }}
+          >
+            <option value="all">ALL</option>
+            {Object.keys(BlogStatus).map((status) => (
+              <option key={status} value={status}>
+                {status.toUpperCase()}
+              </option>
+            ))}
+          </Select>
+        </FormControl>
+      </Flex>
+      {filteredBlogs.map((blog, idx) => (
         <Box key={`blog-${idx}`} backgroundColor="white" p="2" my="2">
           <Flex justify="space-between" align="center">
             <Box>
@@ -33,6 +91,7 @@ const CmsIndex = ({ blogs }) => {
                 {blog.status?.toUpperCase()}
               </StatusTag>
               <MyLink href={`/blog/${blog.slug}`}>{blog.title}</MyLink>
+              <pre>{blog.tags}</pre>
             </Box>
             <Link href={`/cms/write/${blog.id}`}>
               <IconButton icon="edit" aria-label="edit post" />
--- src/cms/blogs/hooks.ts
+++ src/cms/blogs/hooks.ts
@@ -2,6 +2,7 @@ import { useToast } from "@chakra-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BlogInterface } from "src/interfaces/Blog";
+import _ from "lodash";
 
 export function useBlogUpdate() {
   const [isUpdated, setUpdate] = useState(false);
@@ -60,4 +61,49 @@ export function useBlogUpdate() {
     error: isError,
     success: isSuccess,
   };
+}
+
+export function useBlogsFilter(data, filters) {
+  // filter by title
+  // filter by status
+  const [filteredBlogs, setState] = useState(data);
+  useEffect(() => {
+    filterBlogs();
+  }, [data, filters]);
+
+  const filterBlogs = () => {
+    let blogsByStatus = [];
+    let blogsBySearch = [];
+    const searchFilter = filters.search.trim();
+    if (filters.status && filters.status !== "all") {
+      blogsByStatus = _.filter(data, ({ status }) => status === filters.status);
+    } else {
+      blogsByStatus = data;
+    }
+    if (searchFilter && String(searchFilter).length > 0) {
+      const tagsMatch = _.filter(
+        data,
+        ({ tags }) =>
+          _.intersection(tags, searchFilter.toLowerCase().split(" ")).length > 0
+      );
+      console.log({ tagsMatch });
+      const titleMatch = _.filter(
+        data,
+        ({ title }) =>
+          title.toLowerCase().includes(searchFilter.toLowerCase()) ||
+          searchFilter.toLowerCase().includes(title.toLowerCase())
+      );
+      if (tagsMatch.length > 0 && titleMatch.length > 0) {
+        blogsBySearch = _.intersection(tagsMatch, titleMatch);
+      } else {
+        blogsBySearch = tagsMatch.concat(titleMatch);
+      }
+    } else {
+      blogsBySearch = data;
+    }
+    console.log("By Status", blogsByStatus);
+    console.log("By Search", blogsBySearch);
+    setState(_.intersection(blogsBySearch, blogsByStatus));
+  };
+  return { filteredBlogs };
 }
