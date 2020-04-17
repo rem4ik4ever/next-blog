b002d39a9b1b1ff2402c5036702b6c7b66816913
Rem Kim <kim.rmi.93@gmail.com>
2020-04-17 09:25:58 -0400

    remove out
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

---
+++ pages/blog/index.tsx
@@ -0,0 +1,59 @@
+import { NextPage, GetStaticProps } from "next";
+import { Box, Flex, Heading, List, ListItem, Link } from "@chakra-ui/core";
+import NextLink from "next/link";
+import fetch from "node-fetch";
+import { BlogInterface } from "src/interfaces/Blog";
+import { BlogStatus } from "src/enums/BlogStatus";
+
+const BlogIndexPage: NextPage<{
+  blogs: BlogInterface[];
+}> = (props) => {
+  const { blogs } = props;
+  return (
+    <Box>
+      <Flex flexDirection="column" alignItems="center">
+        <Heading as="h1" size="2xl">
+          Table of Contents
+        </Heading>
+        <List>
+          {blogs.map((blog) => (
+            <ListItem key={`/blog/${blog.slug}`}>
+              <NextLink
+                as={`/blog/${blog.slug}`}
+                href={`/blog/${blog.slug}`}
+                passHref
+              >
+                <Link>{blog.title}</Link>
+              </NextLink>
+            </ListItem>
+          ))}
+        </List>
+      </Flex>
+    </Box>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const payload = await (
+      await fetch(`${process.env.HOST_URL}/api/blogs`)
+    ).json();
+    const blogs = payload
+      .filter((blog) => blog.status === BlogStatus.released)
+      .sort((a, b) => {
+        if (a.createdAt > b.createdAt) return -1;
+        if (a.createdAt < b.createdAt) return 1;
+        return 0;
+      });
+    return {
+      props: { blogs },
+    };
+  } catch (error) {
+    console.error(error.message);
+    return {
+      props: { blogs: [] },
+    };
+  }
+};
+
+export default BlogIndexPage;
---
+++ pages_out/cms/files/index.tsx
@@ -0,0 +1,91 @@
+import {
+  Flex,
+  Heading,
+  Text,
+  Image,
+  Link,
+  Box,
+  IconButton,
+  Tooltip,
+} from "@chakra-ui/core";
+import { GetStaticProps } from "next";
+import fetch from "node-fetch";
+
+const FilesPage = ({ files }) => {
+  return (
+    <Flex direction="column">
+      <Heading as="h1" size="md">
+        Files
+      </Heading>
+      <Flex direction="column">
+        {files.map((file, idx) => (
+          <Box
+            key={`file-${idx}`}
+            w="100"
+            backgroundColor="white"
+            borderRadius="8px"
+            p="2"
+            mb="2"
+          >
+            <Flex align="center">
+              <Link href={file.url} mr="2">
+                <Image src={file.url} alt={file.name} w="220px" />
+              </Link>
+              <Box>
+                <Heading as="h2" size="sm">
+                  {file.name}
+                </Heading>
+                <Text color="gray.500">Size: {file.size}</Text>
+                <Text>
+                  URL: <Link href={file.url}>{file.url}</Link>
+                </Text>
+              </Box>
+              <Flex direction="column" justify="space-around" h="100%">
+                <Tooltip
+                  hasArrow
+                  label="Copy URL"
+                  placement="top"
+                  aria-label="copy-tooltip"
+                >
+                  <IconButton
+                    aria-label="delete"
+                    icon="copy"
+                    color="blue.400"
+                    mb="4"
+                  />
+                </Tooltip>
+                <Tooltip
+                  hasArrow
+                  label="Remove file"
+                  placement="bottom"
+                  aria-label="delete-tooltip"
+                >
+                  <IconButton
+                    aria-label="delete"
+                    icon="delete"
+                    color="red.400"
+                  />
+                </Tooltip>
+              </Flex>
+            </Flex>
+          </Box>
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const files = await fetch(`${process.env.HOST_URL}/api/files`);
+    return {
+      props: { files: await files.json() },
+    };
+  } catch (error) {
+    return {
+      props: { files: [] },
+    };
+  }
+};
+
+export default FilesPage;
