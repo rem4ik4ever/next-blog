786b55e4a3e485caa4fda658bdc0855d680dac13
Rem Kim <kim.rmi.93@gmail.com>
2020-04-19 20:04:10 -0400

    cms routes only dev
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

--- pages/cms/files/index.tsx
+++ pages/cms/files/index.tsx
@@ -1,19 +1,14 @@
-import {
-  Flex,
-  Heading,
-  Text,
-  Image,
-  Link,
-  Box,
-  IconButton,
-  Tooltip,
-} from "@chakra-ui/core";
-import {  GetServerSideProps } from "next";
+import { Flex, Heading } from "@chakra-ui/core";
+import { GetServerSideProps } from "next";
 import fetch from "node-fetch";
 import FileUpload from "src/components/FileUpload";
 import FileCard from "src/pages/files/FileCard";
+import ErrorPage from "next/error";
 
 const FilesPage = ({ files }) => {
+  if (process.env.NODE_ENV !== "development") {
+    return <ErrorPage statusCode={404} />;
+  }
   return (
     <Flex direction="column">
       <Heading as="h1" size="md">
@@ -33,11 +28,11 @@ export const getServerSideProps: GetServerSideProps = async () => {
   try {
     const files = await fetch(`${process.env.NOW_URL}/api/files`);
     return {
-      props: { files: await files.json() },
+      props: { files: await files.json() }
     };
   } catch (error) {
     return {
-      props: { files: [] },
+      props: { files: [] }
     };
   }
 };
