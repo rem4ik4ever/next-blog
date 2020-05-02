0f333dd12d4691f84b96951fc8e645030ddb0695
Rem Kim <kim.rmi.93@gmail.com>
2020-05-02 16:33:07 -0400

    fix
    Signed-off-by: Rem Kim <kim.rmi.93@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import { AppProps } from 'next/app';
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+import { AppProps } from 'next/app';
 import BasicLayout from '../src/components/layout';
 import theme from '../theme';
 
