6e85bd42f06b9062c7d4b27c5c39d798028ffc38
Rem Kim <rem4ik4ever@gmail.com>
2021-10-10 00:34:21 -0400

    ads
    Signed-off-by: Rem Kim <rem4ik4ever@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
 import { AppProps } from 'next/app';
 import BasicLayout from '../src/components/layout';
 import theme from '../theme';
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
+    <Head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8862002577533883"
+     crossOrigin="anonymous"></script>
+    </Head>
     <CSSReset />
       <BasicLayout>
         <Component {...pageProps} />
